3556107121740e9311c9add7b6adb638bb68b0da
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2025-12-25 15:15:00 +0100

    updated textt formatting

--- Patches/UpdateName.cs
+++ Patches/UpdateName.cs
@@ -15,7 +15,7 @@ public static class NamePatch
         string pingColour = __instance.GetPing() > 100 ? __instance.GetPing() > 250 ? "red" : "orange" : "green";
         
         string text = $"{__instance.OwningNetPlayer.SanitizedNickName}{(Plugin.KnownPeople.TryGetValue(__instance.OwningNetPlayer.UserId, out string value) ? $" : <color=green>{value}</color>" : "")}\n" +
-                      $"<color={fpsColour}>{__instance.fps}</color> FPS : <color={pingColour}>{__instance.GetPing()}</color> ms\n" +
+                      $"<color={fpsColour}>{__instance.fps}</color> FPS : <color={pingColour}>{__instance.GetPing()}</color> MS\n" +
                       $"{__instance.GetAccountCreationDate().ToShortDateString()}\n" +
                       $"{__instance.GetPlayerMods().Join(" ")}";
         

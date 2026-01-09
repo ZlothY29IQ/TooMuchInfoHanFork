681364587650bfcfa1d32182cd51eeff1791e94c
ZlothY29IQ <211057194+ZlothY29IQ@users.noreply.github.com>
2026-01-09 21:31:40 +0000

    bump version

--- Constants.cs
+++ Constants.cs
@@ -4,5 +4,5 @@ public static class Constants
 {
     public const string PluginGuid    = "hansolo1000falcon.toomuchinfo";
     public const string PluginName    = "TooMuchInfo";
-    public const string PluginVersion = "1.1.6";
+    public const string PluginVersion = "1.1.7";
 }
